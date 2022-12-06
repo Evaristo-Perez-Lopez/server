1f4a5b384ccbf0bb39cbb2c0a2846f304ece0db4
Evaristo Perez <perezlopezevaristo07@outlook.com>
2022-12-06 06:21:56 -0500

    basic Backend app

---
+++ utils/DBHelper.js
@@ -0,0 +1,21 @@
+// Asistente de conexión a la base de datos
+const mysql = require('mysql');
+
+class DBHelper{
+    // Obtener conexión a la base de datos
+    getConn(){
+        let conn = mysql.createConnection({
+            // Configuración de conexión a la base de datos
+            host:'127.0.0.1',
+            port:'3306',
+            user:'testApiExpress',				// Nombre de usuario del administrador de Mysql
+            password:'password',			// Contraseña de administrador de Mysql
+            database:'testApiExpress'  			// Nombre de la base de datos
+        });
+        conn.connect();
+        return conn;
+    }
+}
+
+module.exports = DBHelper;
+
